4f7cfdea266ab17e050b0436cfd62fb490259e9a
mohansky <38560439+mohansky@users.noreply.github.com>
2024-10-09 09:59:42 +0530

    Update route.tsx

--- app/api/formAction/route.tsx
+++ app/api/formAction/route.tsx
@@ -1,10 +1,7 @@
-// import React from "react";
 import { EmailTemplate } from "@/components/email-template";
 import { formSchema } from "@/lib/formValidation";
-import { z } from "zod"; 
-// import { EmailTemplate } from "@/components/email-template"; 
+import { z } from "zod";
 import { Resend } from "resend";
-// import { getErrorMap } from "zod";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -19,25 +16,10 @@ export const onFormAction = async (
   "use server";
   const data = Object.fromEntries(formData);
   const parsed = formSchema.safeParse(data);
-  // const senderName = formData.get("senderName");
-  // const email = formData.get("email");
-  // const message = formData.get("message");
 
-  if (parsed.success) { 
-    // try {
-    //   await resend.emails.send({
-    //     from: "Acme <onboarding@resend.dev>",
-    //     to: "mohansky@gmail.com",
-    //     subject: `Enquiry from ${senderName}`,
-    //     replyTo: email as string,
-    //     react: React.createElement(EmailTemplate, {
-    //       senderName: senderName as string,
-    //       email: email as string,
-    //       message: message as string,
-    //     }),
-    //   });
+  if (parsed.success) {
     await resend.emails.send({
-      from: "Acme <onboarding@resend.dev>",
+      from: "MK <mohansky@gmail.com>",
       to: "mohansky@gmail.com",
       subject: `Enquiry from ${parsed.data.senderName}`,
       replyTo: parsed.data.email as string,
@@ -51,10 +33,6 @@ export const onFormAction = async (
       message: "Form submitted thank you for your interest.",
       email: parsed.data,
     };
-    // } catch (error: unknown) {
-    //   console.log(error);
-    //   return { error: getErrorMap() };
-    // }
   } 
   else {
     return {
@@ -63,28 +41,3 @@ export const onFormAction = async (
     };
   }
 };
-
-
-// import { EmailTemplate } from "@/components/email-template"; 
-// import { Resend } from 'resend';
-
-// const resend = new Resend(process.env.RESEND_API_KEY);
-
-// export async function POST() {
-//   try {
-//     const { data, error } = await resend.emails.send({
-//       from: 'Acme <onboarding@resend.dev>',
-//       to: ['delivered@resend.dev'],
-//       subject: 'Hello world',
-//       react: EmailTemplate({ senderName: 'Mohan', email: 'mohansky@gmail.com', message: 'Hello World' }),
-//     });
-
-//     if (error) {
-//       return Response.json({ error }, { status: 500 });
-//     }
-
-//     return Response.json(data);
-//   } catch (error) {
-//     return Response.json({ error }, { status: 500 });
-//   }
-// }
